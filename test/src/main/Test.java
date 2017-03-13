3751fe7cd120f2dc2d08ac8ddea50b4c4c4f7cb7
bakhytbek <bakhitbek@inbox.ru>
2017-03-13 14:46:10 +0200

    Initial version

--- test/src/main/Test.java
+++ test/src/main/Test.java
@@ -18,14 +18,14 @@ public class Test {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			String url = "jdbc:oracle:thin:@10.0.2.15:1521:ORCL1";
 			Connection con = DriverManager.getConnection(url, "DB", "1");
-			logger.info("Connected to database (to Oracle10).");
+			logger.info("******* Connected to database (to Oracle10). *******");
 			
 
 			String command = "{call DB.PKG_ACCOUNT.TRANSFER_BALANCE(?,?)}";
 			CallableStatement cstmt = con.prepareCall(command);
 			
 			int account_id = 1;
-			double money = 50000.50d;
+			double money = 50.5d;
 			
 			cstmt.setInt(1, account_id);
 			cstmt.setDouble(2, money);
